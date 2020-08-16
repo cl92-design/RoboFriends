2d571b7de7a1d285a649af5e435292c38b7114a7
Chloe <cl92design@gmail.com>
2020-08-16 16:30:38 +0200

    Updates

---
+++ precache-manifest.36170685346313f89ee0700f0224f935.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77098a59f7622937846b3fec376069e4",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/RoboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "cd820e36e267c791e64d",
+    "url": "/RoboFriends/static/css/main.ce18f9b4.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/RoboFriends/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd820e36e267c791e64d",
+    "url": "/RoboFriends/static/js/main.f9407c96.chunk.js"
+  },
+  {
+    "revision": "671d1b49487ff131bdad",
+    "url": "/RoboFriends/static/js/runtime-main.16920bcc.js"
+  }
+]);
